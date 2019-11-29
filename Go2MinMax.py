e081755b750712d95dacb2d26651c9352d79016a
ucfilho <42356381+ucfilho@users.noreply.github.com>
2019-11-28 21:39:07 -0300

    Update Go2MinMax.py

--- Go2MinMax.py
+++ Go2MinMax.py
@@ -10,92 +10,92 @@ from math import *
 '''Rosembrock Function'''
 def Intervalo(dim,fchoice):
     
-    if(fchoice='Rosenbrock'):
+    if(fchoice=='Rosenbrock'):
         MAX=np.repeat(10,dim)
         MIN=np.repeat(-10,dim)
 
     
-    if(fchoice='Sum_of_different_powers'):
+    if(fchoice=='Sum_of_different_powers'):
         MAX=np.repeat(1,dim)
         MIN=np.repeat(-1,dim)
 
-    if(fchoice='Sphere'):
+    if(fchoice=='Sphere'):
         MAX=np.repeat(5.12,dim)
         MIN=np.repeat(-5.12,dim)
 
-    if(fchoice='Easom'):
+    if(fchoice=='Easom'):
         MAX=np.repeat(10,dim) # precisa trocar
         MIN=np.repeat(-10,dim) # precisa trocar
 
-    if(fchoice='Booth'):
+    if(fchoice=='Booth'):
         MAX=np.repeat(10,dim) # precisa trocar
         MIN=np.repeat(-10,dim) # precisa trocar
 
-    if(fchoice='Beale'):
+    if(fchoice=='Beale'):
         MAX=np.repeat(10,dim) # precisa trocar
         MIN=np.repeat(-10,dim) # precisa trocar
 
 ### Unimodais parecidas
 
-    if(fchoice='bohachevsky_function'):
+    if(fchoice=='bohachevsky_function'):
         MAX=np.repeat(10,dim) # precisa trocar
         MIN=np.repeat(-10,dim) # precisa trocar
 
-    if(fchoice='sum_squares_function'):
+    if(fchoice=='sum_squares_function'):
         MAX=np.repeat(10,dim) # precisa trocar
         MIN=np.repeat(-10,dim) # precisa trocar
     
-    if(fchoice='matyas_function'):
+    if(fchoice=='matyas_function'):
         MAX=np.repeat(10,dim) # precisa trocar
         MIN=np.repeat(-10,dim) # precisa trocar
     
-    if(fchoice='mccormick_function'):
+    if(fchoice=='mccormick_function'):
         MAX=np.repeat(10,dim) # precisa trocar
         MIN=np.repeat(-10,dim) # precisa trocar
     
-    if(fchoice='dixon_price_function'):#sum_of_different_powers_function
+    if(fchoice=='dixon_price_function'):#sum_of_different_powers_function
         MAX=np.repeat(10,dim) # precisa trocar
         MIN=np.repeat(-10,dim) # precisa trocar
     
-    if(fchoice='three_hump_camel_function'):#sum_of_different_powers_function
+    if(fchoice=='three_hump_camel_function'):#sum_of_different_powers_function
         MAX=np.repeat(10,dim) # precisa trocar
         MIN=np.repeat(-10,dim) # precisa trocar
 
 ### Funções multimodais
 
-    if(fchoice='Rastrigin'):
+    if(fchoice=='Rastrigin'):
       MAX=np.repeat(5.12,dim)
       MIN=np.repeat(-5.12,dim)
     
-    if(fchoice='Shubert'):
+    if(fchoice=='Shubert'):
         MAX=np.repeat(10,dim) # precisa trocar
         MIN=np.repeat(-10,dim) # precisa trocar
 
-    if(fchoice='Schwefel'):
+    if(fchoice=='Schwefel'):
         MAX=np.repeat(500,dim)
         MIN=np.repeat(-500,dim)
  
-    if(fchoice='Ackley'):
+    if(fchoice=='Ackley'):
         MAX=np.repeat(32,dim)
         MIN=np.repeat(-32,dim)
 
-    if(fchoice='Bukin'):
+    if(fchoice=='Bukin'):
         MAX=np.repeat(10,dim) # precisa trocar
         MIN=np.repeat(-10,dim) # precisa trocar
 
-    if(fchoice='Cross_in_tray'):
+    if(fchoice=='Cross_in_tray'):
         MAX=np.repeat(10,dim) # precisa trocar
         MIN=np.repeat(-10,dim) # precisa trocar
 
-    if(fchoice='Michalewicz'):
+    if(fchoice=='Michalewicz'):
         MAX=np.repeat(np.pi,dim)
         MIN=np.repeat(0,dim)
 
-    if(fchoice='Drop_wave'):
+    if(fchoice=='Drop_wave'):
         MAX=np.repeat(10,dim) # precisa trocar
         MIN=np.repeat(-10,dim) # precisa trocar
 
-    if(fchoice='Six_hump_camel'):
+    if(fchoice=='Six_hump_camel'):
         MAX=np.repeat(10,dim) # precisa trocar
         MIN=np.repeat(-10,dim) # precisa trocar
 
