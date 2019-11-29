a992faabae1790f4d93a988712c676200312f66d
ucfilho <42356381+ucfilho@users.noreply.github.com>
2019-11-29 00:23:22 -0300

    Update GeraIndices.py

--- GeraIndices.py
+++ GeraIndices.py
@@ -110,8 +110,10 @@ def GeraIndices(X,BESTo,FOBESTo,DIo,MAT_INDo,SOMA,TOTAL,syn0_F,
   else:
     CRo=CRo*(1-Fd);#CRo=CRo-Fc
   
-  if(CRo<Fc):CRo=Fc ;  if(Fo<Fc):Fo=Fc
-  if(CRo>1):CRo=1;   if(CRo< Fc): CRo=Fc
+  if(CRo<Fc):CRo=Fc 
+  if(Fo<Fc):Fo=Fc
+  if(CRo>1):CRo=1
+  if(CRo< Fc): CRo=Fc
 
   CRo=(2*CRo+CRa)/3 # para suavizar
   Fo=(3*Fo+Fa)/4 # para suavizar
